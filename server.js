8ef55dfa264677cdac40e2afa10627b64c980a75
Ömer Faruk <omerfarukkirli@gmail.com>
2017-04-26 16:42:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var port 		= process.env.PORT || 3000 ;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) { // This method controls accept a request or not.
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 
 app.get('/',(req,res)=>{
 
